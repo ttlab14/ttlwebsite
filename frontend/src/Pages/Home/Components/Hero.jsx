1d4903ad679c278c00f5c2aaccf60497cf00b9b1
amine <hamdi.amine.dev@gmail.com>
2023-04-18 17:15:04 +0100

    add header video

--- frontend/src/Pages/Home/Components/Hero.jsx
+++ frontend/src/Pages/Home/Components/Hero.jsx
@@ -1,19 +1,29 @@
 import { Link } from "react-router-dom"
 import React from "react"
+import YouTubeToHtml5 from "@thelevicole/youtube-to-html5-loader"
 
 const Hero = ({ handleClickScroll }) => {
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen overflow-x-hidden">
       {/* <img className="w-full hidden sm:block" src="/images/Home/Hero.png" alt="" />
       <img className="w-full sm:hidden" src="/images/Home/Hero Mobile.png" alt="" /> */}
-      <video
+      {/* <video
         autoPlay
         muted
         loop
         className="w-screen h-screen md:object-cover object-fill bg-black opacity-30 z-0"
+        controls="controls"
       >
-        <source src="/videos/header_video.mp4" type="video/mp4" />
-      </video>
+        <source src="https://youtu.be/QlW0iTOAUE4" type="video/mp4" />
+      </video> */}
+      <iframe
+        src="https://www.youtube.com/embed/QlW0iTOAUE4?autoplay=1&showinfo=0&controls=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+        className="w-screen h-screen md:object-cover object-fill bg-black opacity-30 z-0 "
+      ></iframe>
       <div className="absolute top-36 md:top-1/4 w-full 2xl:top-1/3">
         <div className="w-11/12  md:w-3/4 mx-auto">
           <div className="md:w-3/5">
