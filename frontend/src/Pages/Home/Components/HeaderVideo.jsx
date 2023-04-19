3b2207e1ef7a727f2c946965ca888b92a1cb60e8
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2023-04-19 11:20:04 -0700

    Update HeaderVideo.jsx

--- frontend/src/Pages/Home/Components/HeaderVideo.jsx
+++ frontend/src/Pages/Home/Components/HeaderVideo.jsx
@@ -1,18 +1,13 @@
-import React from "react"
+import React from "react";
 
 const HeaderVideo = () => {
   return (
     <div className="w-11/12  md:w-3/4 mx-auto mb-16">
-      <iframe
-        src="https://www.youtube.com/embed/QlW0iTOAUE4?Version=3&autoplay=1&mute=1&enablejsapi=1&showinfo=0&controls=0&loop=1&playlist=QlW0iTOAUE4"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-        className="w-screen h-screen md:object-cover object-fill bg-black opacity-30 z-0 "
-      ></iframe>
+      <video autoPlay muted loop>
+        <source src="https://toggletechlab.s3.us-east-2.amazonaws.com/header_video.mp4" type="video/mp4" />
+      </video>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderVideo
+export default HeaderVideo;
