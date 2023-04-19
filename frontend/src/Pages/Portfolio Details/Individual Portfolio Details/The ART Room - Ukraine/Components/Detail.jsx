3f0969bf85064e6f45713e69a06b1d138ef67bc2
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2023-04-18 20:18:47 -0700

    Update Detail.jsx

--- frontend/src/Pages/Portfolio Details/Individual Portfolio Details/The ART Room - Ukraine/Components/Detail.jsx
+++ frontend/src/Pages/Portfolio Details/Individual Portfolio Details/The ART Room - Ukraine/Components/Detail.jsx
@@ -1,89 +1,101 @@
-import React from "react"
+import React from "react";
 
 const Detail = () => {
   return (
     <div>
       <div className="mb-16">
         <h1 className="text-white text-xl md:text-2xl mb-8">
-          Art Hostel, Kyiv, Ukraine
+         Art Hostel, Kyiv, Ukraine
         </h1>
-
+        
         <p className="text-grey text-base md:text-lg mt-6">
-          Step into the world of a Ukrainian art studio with a new AR portal.
-          The art studio has been transformed into a 3D experience that can be
-          accessed through a web AR walk-through. With just a quick scan of a QR
-          code on the artist’s webpage, you can immerse yourself in our art
-          studio from anywhere in the world.
+        Step into the world of a Ukrainian art studio with a new AR portal. The art
+         studio has been transformed into a 3D experience that can be accessed 
+         through a web AR walk-through. With just a quick scan of a QR code on 
+         the artist’s webpage, you can immerse
+         yourself in our art studio from anywhere in the world.
         </p>
         <p className="text-grey text-base md:text-lg mt-6">
-          Using cutting-edge technology, we have scanned the studio in 3D and
-          created a blender file that has been optimized for AR. This means that
-          you can experience the art studio as if you were physically standing
-          within it. Explore the space, move around and interact with the
-          artworks in a whole new way. See the pieces from different angles,
-          view them up close or step back and appreciate the space they occupy.
+        Using cutting-edge technology, we have scanned the studio in 3D and
+         created a blender file that has been optimized for AR. This means 
+         that you can experience the art studio as if you were physically
+          standing within it. Explore the space, move around and interact 
+          with the artworks in a whole new way. See the pieces from different
+           angles, view them up close or step back and appreciate the
+            space they occupy.
+        
         </p>
         <p className="text-grey text-base md:text-lg mt-6">
-          This web AR experience offers a unique way to explore the artist’s
-          studio and art. It’s an exciting opportunity to connect with their
-          work in a way that was previously impossible. The portal offers an
-          immersive and interactive experience that will transport you there.
+        This web AR experience offers a unique way to explore the artist’s 
+        studio and  art. It’s an exciting opportunity to connect with their 
+        work in a way that was previously impossible. The portal offers an immersive
+         and interactive experience that will transport you there.
         </p>
         <p className="text-grey text-base md:text-lg mt-6">
-          We’re thrilled to offer this experience to audiences, and we hope that
-          it will inspire a new appreciation for the beauty and intricacy of our
-          work. Whether you’re an art enthusiast or just curious about new
-          technology, this AR portal is an experience not to be missed.
-        </p>
-      </div>
-
-      <div className="mb-16">
-        <h1 className="text-white text-xl md:text-2xl mb-8">Try it out</h1>
-      </div>
-      <div className="flex sm:flex-row flex-col gap-5 items-center">
-        <img
-          className="lg:w-7/12 sm:w-6/12 xs:w-9/12 h-full object-contain"
-          src="/images/PortfolioDetails/ArtStudio/ukraine_ar.jpeg"
-          alt=""
-          style={{ width: "30%" }}
-        />
-        <img
-          className="lg:w-7/12 sm:w-6/12 xs:w-9/12 h-full object-contain"
-          src="/images/PortfolioDetails/ArtStudio/QRCode.jpeg"
-          alt=""
-          style={{ width: "23%" }}
-        />
+        We’re thrilled to offer this experience 
+        to audiences, and we hope that it will inspire a new appreciation for 
+        the beauty and intricacy of our work. Whether you’re an art enthusiast or
+         just curious
+         about new technology, this AR portal is an experience not to be missed.
+         </p>
       </div>
 
       <div className="mb-16">
         <h1 className="text-white text-xl md:text-2xl mb-8">
+          Try it out
+        </h1>
+        </div>
+      <div 
+          className="flex sm:flex-row flex-col gap-5 items-center"
+        >
+          <img
+            className="lg:w-7/12 sm:w-6/12 xs:w-9/12 h-full object-contain"
+            src="https://toggletechlab.s3.us-east-2.amazonaws.com/ArtStudio/ukraine_ar.jpeg"
+            alt=""
+            style={{width:"30%"}}
+
+          />
+          <img
+            className="lg:w-7/12 sm:w-6/12 xs:w-9/12 h-full object-contain"
+            src="https://toggletechlab.s3.us-east-2.amazonaws.com/ArtStudio/QRCode.jpeg"
+            alt=""
+            style={{width:"23%"}}
+          />
+        </div>
+        
+      <div className="mb-16">
+        <h1 className="text-white text-xl md:text-2xl mb-8">
           Web AR experience built using Unity Engine
         </h1>
-      </div>
-      <div className="flex sm:flex-row flex-col gap-5 items-center my-5">
+        </div>
+        <div className="flex sm:flex-row flex-col gap-5 items-center my-5">
         <img
-          className="w-full h-full object-fill"
-          src="/images/PortfolioDetails/ArtStudio/art1.png"
-          alt=""
-        />
-      </div>
-      <div className="flex sm:flex-row flex-col gap-5 items-center my-5">
+            className="w-full h-full object-fill"
+            src="https://toggletechlab.s3.us-east-2.amazonaws.com/ArtStudio/art1.PNG"
+            alt=""
+          />
+        </div>
+        <div className="flex sm:flex-row flex-col gap-5 items-center my-5">
         <img
-          className="w-full h-full object-fill"
-          src="/images/PortfolioDetails/ArtStudio/art2.png"
-          alt=""
-        />
-      </div>
+            className="w-full h-full object-fill"
+            src="https://toggletechlab.s3.us-east-2.amazonaws.com/ArtStudio/art2.PNG"
+            alt=""
+          />
+        </div>
 
-      <div className="flex sm:flex-row flex-col gap-5 items-center my-5">
+<div className="flex sm:flex-row flex-col gap-5 items-center my-5">
         <img
-          className="w-full h-full object-fill"
-          src="/images/PortfolioDetails/ArtStudio/art3.png"
-          alt=""
-        />
+            className="w-full h-full object-fill"
+            src="https://toggletechlab.s3.us-east-2.amazonaws.com/ArtStudio/art3.PNG"
+            alt=""
+          />
+        </div>
+        
       </div>
-    </div>
-  )
-}
+      
+    
+  );
+};
 
-export default Detail
+export default Detail;
+
