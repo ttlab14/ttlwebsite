5db1d7497863fbf8f63e4facd59c638809f2f18e
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2023-04-26 00:26:42 -0700

    Update Hero.jsx

--- src/Pages/Portfolio Details/Individual Portfolio Details/RN Training/Components/Hero.jsx
+++ src/Pages/Portfolio Details/Individual Portfolio Details/RN Training/Components/Hero.jsx
@@ -1,3 +1,4 @@
+
 import React from "react"
 
 const Hero = () => {
@@ -5,17 +6,17 @@ const Hero = () => {
     <div className="relative">
       <img
         className="w-full hidden sm:block"
-        src="/images/PortfolioDetails/Hero.png"
+        src="https://toggletechlab.s3.us-east-2.amazonaws.com/Mobile/Hero-min.png"
         alt=""
       />
       <img
         className="w-full sm:hidden"
-        src="/images/PortfolioDetails/HeroMobile.png"
+        src="https://toggletechlab.s3.us-east-2.amazonaws.com/Mobile/Hero+Mobile.png"
         alt=""
       />
       <div className="absolute top-1/3 mt-8 w-full">
         <h1 className="w-11/12  md:w-3/4 mx-auto text-2xl md:text-4xl text-white">
-          RN Training
+          VR RN Training
           <br />
         </h1>
       </div>
