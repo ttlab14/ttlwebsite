78af1428d652f1d011e0ceb79664e60023d9a65a
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2023-04-25 19:16:11 -0700

    Update Team.jsx

--- src/Pages/About/Components/Team.jsx
+++ src/Pages/About/Components/Team.jsx
@@ -7,49 +7,49 @@ const Team = () => {
       <div className="grid grid-cols-2 lg:grid-cols-4">
         <div className="flex flex-col items-center md:justify-center mb-12">
           <div className="w-32 h-32  md:w-52 md:h-52 rounded-full overflow-hidden">
-            <img src="/images/Team/sukhi.jpeg" alt="" />
+            <img src="https://toggletechlab.s3.us-east-2.amazonaws.com/Team/sukhi.jpeg" alt="Toggle Tech Lab Team" />
           </div>
           <h1 className="text-white text-xl mt-4 mb-1 text-center">Sukhwinder</h1>
           <p className="text-gold text-base md:text-lg text-center">Founder / CEO</p>
         </div>
         <div className="flex flex-col items-center md:justify-center mb-12">
           <div className="w-32 h-32  md:w-52 md:h-52 rounded-full overflow-hidden">
-            <img src="/images/Team/Team-2.png" alt="" />
+            <img src="https://toggletechlab.s3.us-east-2.amazonaws.com/Team/Team-2.png" alt="Toggle Tech Lab Team" />
           </div>
           <h1 className="text-white text-xl mt-4 mb-1 text-center">Gurfariyad</h1>
           <p className="text-gold text-base md:text-lg text-center">Co-Founder</p>
         </div>
         <div className="flex flex-col items-center md:justify-center mb-12">
           <div className="w-32 h-32  md:w-52 md:h-52 rounded-full overflow-hidden">
-            <img src="/images/Team/bikram.jpg" alt="" />
+            <img src="https://toggletechlab.s3.us-east-2.amazonaws.com/Team/bikram.jpg" alt="Toggle Tech Lab Team" />
           </div>
           <h1 className="text-white text-xl mt-4 mb-1 text-center">Bikram</h1>
           <p className="text-gold text-base md:text-lg text-center">VR & AR Developer</p>
         </div>
         <div className="flex flex-col items-center md:justify-center mb-12">
           <div className="w-32 h-32  md:w-52 md:h-52 rounded-full overflow-hidden">
-            <img src="/images/Team/venky.jpg" alt="" />
+            <img src="https://toggletechlab.s3.us-east-2.amazonaws.com/Team/venky.jpg" alt="Toggle Tech Lab Team" />
           </div>
           <h1 className="text-white text-xl mt-4 mb-1 text-center">Venkatdari</h1>
           <p className="text-gold text-base md:text-lg text-center">VR & AR  Developer</p>
         </div>
         <div className="flex flex-col items-center md:justify-center mb-12">
           <div className="w-32 h-32  md:w-52 md:h-52 rounded-full overflow-hidden">
-            <img src="/images/Team/brian.jpg" alt="" />
+            <img src="https://toggletechlab.s3.us-east-2.amazonaws.com/Team/brian.jpg" alt="Toggle Tech Lab Team" />
           </div>
           <h1 className="text-white text-xl mt-4 mb-1 text-center">Brian</h1>
           <p className="text-gold text-base md:text-lg text-center">Game developer</p>
         </div>
         <div className="flex flex-col items-center md:justify-center mb-12">
           <div className="w-32 h-32  md:w-52 md:h-52 rounded-full overflow-hidden">
-            <img src="/images/Team/sonya.jpg" alt="" />
+            <img src="/https://toggletechlab.s3.us-east-2.amazonaws.com/Team/sonya.jpg" alt="Toggle Tech Lab Team" />
           </div>
           <h1 className="text-white text-xl mt-4 mb-1 text-center">Sonya</h1>
           <p className="text-gold text-base md:text-lg text-center">XR User Experience Designer</p>
         </div>
         <div className="flex flex-col items-center md:justify-center mb-12">
           <div className="w-32 h-32  md:w-52 md:h-52 rounded-full overflow-hidden">
-            <img src="/images/Team/rohit.png" alt="" />
+            <img src="https://toggletechlab.s3.us-east-2.amazonaws.com/Team/rohit.PNG" alt="Toggle Tech Lab Team" />
           </div>
           <h1 className="text-white text-xl mt-4 mb-1 text-center">Rohit</h1>
           <p className="text-gold text-base md:text-lg text-center">3D Designer</p>
