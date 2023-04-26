6c313fe4501cd4cb4bcb75054aa9cd34d1c98bff
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2023-04-26 00:33:32 -0700

    Update Hero.jsx

--- src/Pages/Portfolio Details/Individual Portfolio Details/Inverse Surveillance/Components/Hero.jsx
+++ src/Pages/Portfolio Details/Individual Portfolio Details/Inverse Surveillance/Components/Hero.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React from "react"
 
 const Hero = () => {
   return (
     <div className="relative">
-      <img className="w-full hidden sm:block" src="https://toggletechlab.s3.us-east-2.amazonaws.com/PortfolioPage/Hero-min.png" alt="" />
-      <img className="w-full sm:hidden" src="https://toggletechlab.s3.us-east-2.amazonaws.com/PortfolioPage/Hero+Mobile-min.png" alt="" />
+      <img
+        className="w-full hidden sm:block"
+        src="https://toggletechlab.s3.us-east-2.amazonaws.com/Mobile/Hero-min.png"
+        alt=""
+      />
+      <img
+        className="w-full sm:hidden"
+        src="https://toggletechlab.s3.us-east-2.amazonaws.com/Mobile/Hero+Mobile.png"
+        alt=""
+      />
       <div className="absolute top-1/3 mt-8 w-full">
         <h1 className="w-11/12  md:w-3/4 mx-auto text-2xl md:text-4xl text-white">
           The Inverse Surveillance Project
@@ -12,7 +20,7 @@ const Hero = () => {
         </h1>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
