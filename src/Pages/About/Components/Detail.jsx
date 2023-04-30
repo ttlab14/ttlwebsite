fe8d822060f5e62867b6182d56587db6fe8980cc
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2023-04-30 00:56:23 -0700

    Update Detail.jsx

--- src/Pages/About/Components/Detail.jsx
+++ src/Pages/About/Components/Detail.jsx
@@ -13,19 +13,17 @@ const Detail = () => {
             Prototypes And Products That Get It
           </h1>
           <p className="text-grey text-base md:text-lg">
-          Welcome to Toggle Tech Lab, the premier destination for turning your concepts into tangible 
-          XR products or services. Since 2018 we have helped over 115 startups and businesses.
+         Welcome to Toggle Tech Lab XR solutions, your go-to XR solutions provider for all your business needs. Our expert team specializes in Augmented reality development, Virtual reality development, and Mixed reality development to cater to a wide range of industries, including but not limited to, health tech, corporate real estate, and edutech.
           </p> 
           <p className="text-grey text-base md:text-lg">
-
-          At Toggle Tech Lab, we are dedicated to helping both 
-          startups and established businesses achieve their goals by creating cutting-edge
-           virtual and augmented reality prototypes. Our team of highly skilled developers 
-           and designers utilizes the latest technology to deliver fully immersive experiences
-            that bring your vision to life. With our expertise, you can be confident that your
-             project will be executed with precision and excellence. Don't settle for anything less 
-             than the best. Join us at Toggle Tech Lab and let us help you transform your ideas into reality.
+          We provide personalized guidance to startups and SMEs and offer affordable XR development solutions to turn your concepts into reality. Our rapid prototyping XR services enable you to test your ideas quickly and efficiently. We also offer custom AR/VR solutions to create immersive and interactive experiences for investor presentations and training and education purposes.
           </p>
+          <p className="text-grey text-base md:text-lg">
+            At Toggle Tech Lab XR solutions, we understand the importance of precision and excellence in the XR industry. That's why our highly skilled team of developers and designers uses the latest technology to deliver extended reality services, immersive technology solutions, and holographic experiences that exceed your expectations.
+  </p>
+          <p className="text-grey text-base md:text-lg">
+          We are dedicated to helping both startups and established businesses achieve their goals by creating cutting-edge virtual and augmented reality prototypes. Don't settle for anything less than the best. Join us at Toggle Tech Lab and let us help you transform your ideas into reality.
+  </p>
         </div>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-12 items-center my-20">
