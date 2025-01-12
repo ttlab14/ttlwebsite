9ac33f467fb2ff811390ec43e17c0e430df6d24c
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2025-01-11 22:46:10 -0800

    Update Team.jsx

--- Components/about/Team.jsx
+++ Components/about/Team.jsx
@@ -6,12 +6,7 @@ const Team = () => {
       <h1 className="text-white text-xl md:text-2xl mb-8">Our Team</h1>
      
   
-          Our mission is to empower startups to take their ideas to the next
-          level by providing them with the tools and expertise to create amazing
-          XR prototypes. We believe that XR technology has the power to
-          transform the way we interact with the world, and we are committed to
-          helping our clients leverage its full potential.
-       
+
     
   );
 };
