2121ecb80c93d3a6d2f6334beeb6b4e8f5156f34
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2025-01-11 22:34:03 -0800

    Update Team.jsx

--- Components/about/Team.jsx
+++ Components/about/Team.jsx
@@ -5,18 +5,7 @@ const Team = () => {
   return (
       <h1 className="text-white text-xl md:text-2xl mb-8">Our Team</h1>
      
-        <div className="md:w-11/12">
-         
-          <p className="text-grey text-base md:text-lg">
-            Welcome to Toggle Tech Lab XR solutions, your go-to XR solutions
-            provider for all your business needs. Our expert team specializes in
-            Augmented reality development, Virtual reality development, and
-            Mixed reality development to cater to a wide range of industries,
-            including but not limited to, health tech, corporate real estate,
-            and edutech.
-          </p>
         
-        </div>
    
   
     
