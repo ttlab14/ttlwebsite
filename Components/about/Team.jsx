a58e0e245777a898011dd9d50a0b52252657fa0a
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2025-01-11 22:29:45 -0800

    Update Team.jsx

--- Components/about/Team.jsx
+++ Components/about/Team.jsx
@@ -5,7 +5,9 @@ const Team = () => {
   return (
       <h1 className="text-white text-xl md:text-2xl mb-8">Our Team</h1>
      
-
+     <p className="text-grey text-base md:text-lg">
+           Our team is a dynamic blend of skilled professionals, including two operational managers, talented designers, developers, machine learning engineers, and UX/UI experts. We work collaboratively to deliver innovative and effective solutions that cater to our clients' unique needs. With a strong focus on design excellence, cutting-edge technology, and seamless user experiences, our team is dedicated to driving success and creating impactful results in every project we undertake.
+          </p>
   
     
   );
