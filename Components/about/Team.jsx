3a25a3ae7f5160954e7a2ab1e89e3a0b53284f0d
ttlab14 <131038214+ttlab14@users.noreply.github.com>
2025-01-11 22:40:59 -0800

    Update Team.jsx

--- Components/about/Team.jsx
+++ Components/about/Team.jsx
@@ -5,8 +5,9 @@ const Team = () => {
   return (
       <h1 className="text-white text-xl md:text-2xl mb-8">Our Team</h1>
      
-        <p>hi</p>
-   
+ <p className="text-grey text-base md:text-lg">
+              The faces behind Toggle Tech Lab are seasoned professionals
+            </p>   
   
     
   );
